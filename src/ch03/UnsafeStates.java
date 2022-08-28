aa3912f3de50659b2c49e4b62bd624e2e7fc7083
MARI <mari20201101@gmail.com>
2022-08-28 16:21:00 +0900

    Practice using latch for concurrency

---
+++ src/ch03/UnsafeStates.java
@@ -0,0 +1,12 @@
+package ch03;
+
+public class UnsafeStates {
+
+    private String[] states = new String[]{
+      "a","b","c"
+    };
+
+    public String[] getStates() {
+        return states;
+    }
+}
